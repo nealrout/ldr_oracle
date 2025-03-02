f571e74c8f3029575357878a72864dd564404486
nealrout <nroutson@gmail.com>
2025-03-02 15:04:07 -0500

    .

--- sql/FK_DROP.sql
+++ sql/FK_DROP.sql
@@ -23,6 +23,18 @@ BEGIN
         END IF;
     END LOOP;
 
+    FOR c IN (SELECT COUNT(*) cnt
+              FROM user_constraints
+              WHERE constraint_name = 'FK_LOAD_CONFIG_LOAD_TYPE_LOAD_TYPE_CODE'
+              AND table_name = 'LOAD_CONFIG') LOOP
+        IF c.cnt > 0 THEN
+            EXECUTE IMMEDIATE 'ALTER TABLE LOAD_CONFIG DROP CONSTRAINT FK_LOAD_CONFIG_LOAD_TYPE_LOAD_TYPE_CODE';
+            DBMS_OUTPUT.PUT_LINE('Foreign key constraint FK_LOAD_CONFIG_LOAD_TYPE_LOAD_TYPE_CODE has been dropped.');
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('Foreign key constraint FK_LOAD_CONFIG_LOAD_TYPE_LOAD_TYPE_CODE does not exist.');
+        END IF;
+    END LOOP;
+
     -- FOR c IN (SELECT COUNT(*) cnt
     --           FROM user_constraints
     --           WHERE constraint_name = 'FK_ACCOUNT_ID'
