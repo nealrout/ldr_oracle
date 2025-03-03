9e560b2226215ef0f3b09f71185259324be5980b
nealrout <nroutson@gmail.com>
2025-03-03 07:03:07 -0500

    .

--- sql/row/ROW_LOAD_CONFIG.sql
+++ sql/row/ROW_LOAD_CONFIG.sql
@@ -1,6 +1,6 @@
 BEGIN
     INSERT INTO LOAD_CONFIG(STEP_CODE, SRC_TABLE, TGT_TABLE, CONFIG, CREATE_TS)
-    VALUES ('PROCESS_MAPPED', 'STG_INPUT_01', 'STG_TRANSFORM_01', 
+    VALUES ('PROCESS_MAPPED', 'STG_INPUT_01', 'STG_MAP_01', 
             '{"mappings":
                 [
                     {"srcField":"FIELD_001","tgtField":"FIELD_001"},
@@ -18,7 +18,8 @@ BEGIN
                 [
                     {"fieldName": "FIELD_003", "validation":"DBMS_LOB.GETLENGTH(FIELD_003) < 20"}, 
                     {"fieldName": "FIELD_005", "validation":"REGEXP_LIKE(DBMS_LOB.SUBSTR(FIELD_005, 100, 1), ''^[+-]?\\d+(\\.\\d+)?$'')"}
-                ]
+                ],
+            "uniqueIdentifier":"FIELD_001"
             }', 
             SYSTIMESTAMP);
 
